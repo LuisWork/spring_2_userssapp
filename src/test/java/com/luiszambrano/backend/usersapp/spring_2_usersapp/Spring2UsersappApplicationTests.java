827a03496c5a82b2fa48140ef7c3d679f165c0d5
LuisWork <luiszambrano.work@gmail.com>
2025-04-01 17:17:56 -0300

    Primer Commit

---
+++ src/test/java/com/luiszambrano/backend/usersapp/spring_2_usersapp/Spring2UsersappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiszambrano.backend.usersapp.spring_2_usersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2UsersappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
