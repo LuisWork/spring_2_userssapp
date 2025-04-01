e614c3bc9bacd53958f4f6083e4de76516c3db59
LuisWork <luiszambrano.work@gmail.com>
2025-04-01 17:33:21 -0300

    Implementando UserRepository

---
+++ src/main/java/com/luiszambrano/backend/usersapp/spring_2_usersapp/models/entities/UserRepository.java
@@ -0,0 +1,7 @@
+package com.luiszambrano.backend.usersapp.spring_2_usersapp.models.entities;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
