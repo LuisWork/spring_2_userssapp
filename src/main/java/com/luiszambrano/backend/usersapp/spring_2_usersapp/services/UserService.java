f06ab398f34effd8e37e4b8e3be0ff0346c4c755
LuisWork <luiszambrano.work@gmail.com>
2025-04-01 17:41:04 -0300

    Implementando UserService y UserServiceImpl

---
+++ src/main/java/com/luiszambrano/backend/usersapp/spring_2_usersapp/services/UserService.java
@@ -0,0 +1,18 @@
+package com.luiszambrano.backend.usersapp.spring_2_usersapp.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.luiszambrano.backend.usersapp.spring_2_usersapp.models.entities.User;
+
+public interface UserService {
+
+    List<User> findAll();
+
+    Optional<User> findById(Long id);
+
+    User save(User user);
+
+    void remove(Long id);
+
+}
