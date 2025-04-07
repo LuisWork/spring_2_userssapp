e7b581ff14253fcb76c2a710d0c98bba53f12d84
LuisWork <luiszambrano.work@gmail.com>
2025-04-07 17:40:50 -0400

    Implementando Validaciones en el Backend POST PUT

--- src/main/java/com/luiszambrano/backend/usersapp/spring_2_usersapp/controllers/UserController.java
+++ src/main/java/com/luiszambrano/backend/usersapp/spring_2_usersapp/controllers/UserController.java
@@ -1,11 +1,14 @@
 package com.luiszambrano.backend.usersapp.spring_2_usersapp.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.luiszambrano.backend.usersapp.spring_2_usersapp.models.entities.User;
 import com.luiszambrano.backend.usersapp.spring_2_usersapp.services.UserService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/users")
@@ -42,12 +47,18 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<?> create(@RequestBody User user) {
+    public ResponseEntity<?> create(@Valid @RequestBody User user, BindingResult result) {
+        if(result.hasErrors()) {
+            return validation(result);
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(user));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> update(@RequestBody User user, @PathVariable Long id) {
+    public ResponseEntity<?> update(@Valid @RequestBody User user, BindingResult result, @PathVariable Long id) {
+        if(result.hasErrors()) {
+            return validation(result);
+        }
         Optional<User> o = service.update(user, id);
         if(o.isPresent()) {
             return ResponseEntity.status(HttpStatus.CREATED).body(o.orElseThrow());
@@ -63,6 +74,14 @@ public class UserController {
             return ResponseEntity.noContent().build();
         }
         return ResponseEntity.noContent().build();
+    }
+
+    private ResponseEntity<?> validation(BindingResult result) {
+        Map<String, String> errors = new HashMap<>();
+        result.getFieldErrors().forEach(err -> {
+            errors.put(err.getField(), "El campo " + err.getField() + " " + err.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errors);
     }
 
 }
